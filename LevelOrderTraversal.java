fd1fd8d6f0ca2a0d3b855b7b5d274693a07d4fe0
amannitp <amannitp272@gmail.com>
2022-09-05 14:16:34 +0530

    Level order Traversal in generic Tree

--- LevelOrderTraversal.java
+++ LevelOrderTraversal.java
@@ -8,68 +8,7 @@ class LevelOrderTraversal{
         ArrayList<Node> children=new ArrayList<>();
     }
     
-    public static void display(Node root){
-    
-        String ans="";
-        ans+=root.val;
-        ans+="->";
-        
-        for(int i=0;i<root.children.size();i++){
-            ans+=root.children.get(i).val;
-            ans+=",";
-        }
-        ans+=".";
-        System.out.println(ans);
-        
-        for(int i=0;i<root.children.size();i++){
-            display(root.children.get(i));
-        }
-        
-    }
-    
-    public static int size(Node root){
-        
-        int ans=0;
-        for(int i=0;i<root.children.size();i++){
-             ans+= size(root.children.get(i));
-        }
-        
-        return ans+1;
-    }
-    
-    public static int getMaximun(Node root){
-        
-        int max=Integer.MIN_VALUE;
-        
-        for(int i=0;i<root.children.size();i++){
-            
-            max=Math.max(getMaximun(root.children.get(i)),max);
-        }
-        return Math.max(max,root.val);
-    }
-    
-    public static int heigthOfTree(Node root){
-        
-        int h=-1;
-        
-        for(int i=0;i<root.children.size();i++){
-            
-            h=Math.max(heigthOfTree(root.children.get(i)),h);
-        }
-        return h+1;
-    }
-    
-    // depth first search 
-    public static void traversalOfTree(Node root){
-        
-        System.out.println(" Node Pre "+root.val);
-        
-        for(int i=0;i<root.children.size();i++){
-            System.out.println("Node Edge "+root.children.get(i).val);
-            traversalOfTree(root.children.get(i));
-            System.out.println("Node Post "+root.children.get(i).val);
-        }
-    }
+    // Breadth first search
     public static void levelOrderTraversal(Node root){
         
         Queue<Node> q=new LinkedList<>();
